be6a183164e382ee574fdf247943eeef4e77e6df
homerlynn <wyh900207@126.com>
2019-06-04 14:36:53 +0800

    Initial Commit

---
+++ DJMainComDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DJMainComDemo
+//
+//  Created by DJAPPLE3-ysy on 2019/6/4.
+//  Copyright © 2019 djkg.ylhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DJMainComDemoTests/DJMainComDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  DJMainComDemoTests.m
+//  DJMainComDemoTests
+//
+//  Created by DJAPPLE3-ysy on 2019/6/4.
+//  Copyright © 2019 djkg.ylhy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DJMainComDemoTests : XCTestCase
+
+@end
+
+@implementation DJMainComDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
